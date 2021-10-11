1fae05c81e9c0a2803097774023c7281d43211f9
Alexander Robert <allirobe@ucsc.edu>
2021-10-11 16:13:37 -0700

    Added new pair prototype shell

---
+++ docs/PairProject/main.js
@@ -0,0 +1,16 @@
+//Pair Prototype Project
+//Authors: Alexander Robert and Milo Fisher
+
+title = "";
+
+description = `
+`;
+
+characters = [];
+
+options = {};
+
+function update() {
+  if (!ticks) {
+  }
+}
