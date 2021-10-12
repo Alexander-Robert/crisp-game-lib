2aa86c45503fdc256964eb197135783235b9e5fc
MiloFisher <tbfmilo@gmail.com>
2021-10-11 23:13:03 -0700

    Began work on generateRoom function

--- docs/PairProject/main.js
+++ docs/PairProject/main.js
@@ -17,11 +17,17 @@ const SETTINGS = {
 // Global Variables
 let roomLayout;
 let currentRoom;
+let startRoomTypes;
+let roomTypes;
 
 // Called once upon initialization
 function start() {
+  startRoomTypes = [0]; // Store all variations of the start room in this array
+  roomTypes = [new t0()];
   roomLayout = [];
   currentRoom = generateRoom();
+  currentRoom = generateRoom();
+  console.log(roomLayout);
 }
 
 // Called every frame, 60 fps
@@ -33,7 +39,138 @@ function update() {
 
 // Generates and returns a room in the layout
 function generateRoom() {
+  // If currentRoom is undefined, generate the first room
+  if(currentRoom == undefined) {
+    addRows(2);
+    var r = new room(startRoomTypes[rndi(0,startRoomTypes.length - 1)], 0, 0);
+    roomLayout[0][0] = r;
+    return r;
+  }
+  // Otherwise, find exit of current room and spawn a room with a corresponding entrance
+  var exit = roomTypes[currentRoom.type].exit;
+  if (currentRoom.row == roomLayout.length - 1 && exit == "north") {
+    addRows(2);
+  }
+  var targetPosition;
+  var requiredOrientation;
+  var requiredHalf;
+  var requiredEntrance;
+  var requiredExit;
+  var acceptableRoomOrientations = ["1x1"];
 
+  switch(exit) {
+    case "north": 
+      requiredEntrance = "south";
+      // ***Still need to define***
+      break;
+    case "west":
+      requiredEntrance = "east";
+      targetPosition = vec(currentRoom.row, currentRoom.col - 1);
+      // Test for acceptable orientations
+      if (targetPosition.y > 0) {
+        acceptableRoomOrientations.push("1x2");
+      } else {
+        acceptableRoomOrientations.push("2x1");
+      }
+      // Randomly choose an acceptable orientation
+      requiredOrientation = acceptableRoomOrientations[rndi(0, acceptableRoomOrientations.length - 1)];
+      switch (requiredOrientation) {
+        case "1x1":
+          if (targetPosition.y > 0) {
+            requiredExit = "west";
+          } else {
+            requiredExit = "north";
+          }
+          requiredHalf = undefined;
+          break;
+        case "1x2":
+          requiredExit = "north";
+          requiredHalf = "right";
+          break;
+        case "2x1":
+          requiredExit = "east";
+          requiredHalf = "bottom";
+          break;
+      }
+      break;
+    case "east":
+      requiredEntrance = "west";
+      targetPosition = vec(currentRoom.row, currentRoom.col + 1);
+      // Test for acceptable orientations
+      if(targetPosition.y < SETTINGS.LAYOUT_WIDTH - 1) {
+        acceptableRoomOrientations.push("1x2");
+      } else {
+        acceptableRoomOrientations.push("2x1");
+      }
+      // Randomly choose an acceptable orientation
+      requiredOrientation = acceptableRoomOrientations[rndi(0,acceptableRoomOrientations.length-1)];
+      switch (requiredOrientation) {
+        case "1x1":
+          if (targetPosition.y < SETTINGS.LAYOUT_WIDTH - 1) {
+            requiredExit = "east";
+          } else {
+            requiredExit = "north";
+          }
+          requiredHalf = undefined;
+          break;
+        case "1x2":
+          requiredExit = "north";
+          requiredHalf = "left";
+          break;
+        case "2x1":
+          requiredExit = "west";
+          requiredHalf = "bottom";
+          break;
+      }
+      break;
+  }
+
+  // Adds all eligible room types to a list that we can choose from
+  var eligibleRoomTypes = [];
+  for(var i = 0; i < roomTypes.lenght; i++) {
+    if (roomTypes[i].orientation == requiredOrientation && roomTypes[i].half == requiredHalf && roomTypes[i].entrance == requiredEntrance && roomTypes[i].exit == requiredExit) {
+      eligibleRoomTypes.push(roomTypes[i]);
+    }
+  }
+  if(eligibleRoomTypes.length == 0) {
+    console.log("Not enough eligible room types to generate room:");
+    console.log("Required orientation: " + requiredOrientation);
+    console.log("Required half: " + requiredHalf);
+    console.log("Required entrance: " + requiredEntrance);
+    console.log("Required exit: " + requiredExit);
+    return undefined;
+  }
+  
+  // Randomly select type and fill in roomLayout
+  var selectedType = eligibleRoomTypes[rndi(0, eligibleRoomTypes.length - 1)];
+  switch(selectedType.half) {
+    case undefined:
+      var r = new room(selectedType, targetPosition.x, targetPosition.y);
+      roomLayout[targetPosition.x][targetPosition.y] = r;
+      return r;
+    case "bottom":
+      var r = new room(selectedType, targetPosition.x, targetPosition.y);
+      roomLayout[targetPosition.x][targetPosition.y] = r;
+      r = new room(selectedType + 1, targetPosition.x, targetPosition.y + 1);
+      roomLayout[targetPosition.x][targetPosition.y + 1] = r;
+      return r;
+    case "left":
+      var r = new room(selectedType, targetPosition.x, targetPosition.y);
+      roomLayout[targetPosition.x][targetPosition.y] = r;
+      r = new room(selectedType + 1, targetPosition.x + 1, targetPosition.y);
+      roomLayout[targetPosition.x + 1][targetPosition.y] = r;
+      return r;
+    case "right":
+      var r = new room(selectedType, targetPosition.x, targetPosition.y);
+      roomLayout[targetPosition.x][targetPosition.y] = r;
+      r = new room(selectedType - 1, targetPosition.x - 1, targetPosition.y);
+      roomLayout[targetPosition.x - 1][targetPosition.y] = r;
+      return r;
+    case "top":
+      console.log("Should not be reachable");
+      return undefined;
+  }
+  
 }
 
 // Adds (X) amount of empty rows at the top of the map
@@ -46,22 +183,29 @@ function addRows(amount) {
   }
 }
 
-/* Single Rooms:
- * TYPE: the numerical value that represents how the room will physically be drawn
- * Row, Col: the row and column coordinates in the roomLayout 2D array for the room
+/* Rooms:
+ * TYPE: (integer) the numerical value used to identify which room type is associated with it
+ * Row, Col: (integers) the row and column coordinates in the roomLayout 2D array for the room
  */
 function room(type, row, col) {
   this.type = type;
-  this.pos = vec(row,col);
+  this.row = row;
+  this.col = col;
 }
 
-/* Combo Rooms:
- * TYPE: the numerical value that represents how the room will physically be drawn, type + 1
- *       represents second half of a combo room since each half will be drawn individually
- * Row A, Col A: the row and column coordinates in the roomLayout 2D array for the first half
- * Row B, Col B: the row and column coordinates in the roomLayout 2D array for the second half
+/* Custom Room Types (tX):
+ * Here is where custom rooms are defined.  Make sure that any definitions down here
+ * are added to the array roomTypes in the start() function.
+ * 
+ * Additional Info:
+ * For 1x2: entrance will always be on one half, and the exit on the other half
+ * For 2x1: the entrance will always be on the bottom half, and the exit will be on the top half
+ * Both halves of 1x2 and 2x1 list both the entrance and exit directions
+ * Left and Bottom are both listed before Right and Top
  */
-function comboRoom(type, rowA, colA, rowB, colB) {
-  this.roomA = new room(type, rowA, colA);
-  this.roomB = new room(type + 1, rowB, colB);
+function t0() {
+  this.entrance = undefined; // south, east, or west *Note no north entrances allowed**
+  this.exit = "east"; // north, east, or west **Note no south exits allowed**
+  this.orientation = "1x1"; // 1x1, 1x2, or 2x1
+  this.half = undefined; // undefined, left, right, top, or bottom
 }
